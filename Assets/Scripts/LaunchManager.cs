39a59b02a73ac9dec89e5bbf6424c88438326243
Behlul <behlul_gulmez@hotmail.com>
2020-04-09 15:36:55 +0300

    First upload

---
+++ Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+
+    public void ConnectToPhotonServer()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.NickName + "Connected to photon server.");
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to internet.");
+    }
+
+
+
+
+}

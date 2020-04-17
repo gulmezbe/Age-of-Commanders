99d285f3a88fef449f2d6b5d2a2ec01ae9562416
Behlul <gulmezbe@itu.edu.tr>
2020-04-17 03:30:30 +0300

    ortak odaya baglanma

--- Assets/Scripts/LaunchManager.cs
+++ Assets/Scripts/LaunchManager.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class LaunchManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject EnterGamePanel;
+    public GameObject ConnectionStatusPanel;
+    public GameObject LobbyPanel;
+
+    private void Awake()
     {
-        
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        EnterGamePanel.SetActive(true);
+        ConnectionStatusPanel.SetActive(false);
+        LobbyPanel.SetActive(false);
+    }
+
     void Update()
     {
                 
@@ -22,17 +32,57 @@ public class LaunchManager : MonoBehaviourPunCallbacks
         if (!PhotonNetwork.IsConnected)
         {
             PhotonNetwork.ConnectUsingSettings();
+            ConnectionStatusPanel.SetActive(true);
+            EnterGamePanel.SetActive(false);
         }
+    }
+
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log(PhotonNetwork.NickName + "Connected to photon server.");
+        LobbyPanel.SetActive(true);
+        ConnectionStatusPanel.SetActive(false);
     }
 
     public override void OnConnected()
     {
         Debug.Log("Connected to internet.");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log(message);
+        CreateAndJoinRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.NickName + " joined to" + PhotonNetwork.CurrentRoom.Name + " " + PhotonNetwork.CurrentRoom.PlayerCount);
+        PhotonNetwork.LoadLevel("GameScene");
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " joined to" + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room " + Random.Range(0, 10000);
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+
     }
 
 

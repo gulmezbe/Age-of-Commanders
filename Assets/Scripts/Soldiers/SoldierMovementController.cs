6f18e5b2975ef946756ae93ff6ac7809d5bdff98
Behlul <behlul_gulmez@hotmail.com>
2020-06-18 20:11:02 +0300

    askerler hasar aliyor
    askerler hasar aliyor

--- Assets/Scripts/Soldiers/SoldierMovementController.cs
+++ Assets/Scripts/Soldiers/SoldierMovementController.cs
@@ -6,10 +6,14 @@ using Photon.Realtime;
 
 public class SoldierMovementController : MonoBehaviourPunCallbacks
 {
-    float moveSpeedRed = 1f;
-    float moveSpeedBlue = -1f;
+    bool isDead = false;
+    float moveSpeedRed = 1.0f;
+    float moveSpeedBlue = -1.0f;
     float soldierHealth = 100f;
     float soldierRange = 1.5f;
+    float soldierDamage = 100f;
+    float attackRate = 5.0f;
+    float attackTimer;
 
     GameObject[] otherSoldiers;
 
@@ -21,8 +25,10 @@ public class SoldierMovementController : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        float horizontalMovementRed = moveSpeedRed * Time.deltaTime;
-        float horizontalMovementBlue = moveSpeedBlue * Time.deltaTime;
+        attackTimer = 0.0f;
+
+        float horizontalMovementRed = moveSpeedRed * Time.fixedDeltaTime;
+        float horizontalMovementBlue = moveSpeedBlue * Time.fixedDeltaTime;
 
         directionOfMovementRed = new Vector3(horizontalMovementRed, 0, 0);
         directionOfMovementBlue = new Vector3(horizontalMovementBlue, 0, 0);
@@ -33,15 +39,32 @@ public class SoldierMovementController : MonoBehaviourPunCallbacks
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (soldierHealth <= 0f)
+        {
+            Die();
+        }
+
+        if (attackTimer < attackRate)
+        {
+            attackTimer += Time.fixedDeltaTime;
+        }
+
         otherSoldiers = GameObject.FindGameObjectsWithTag("Soldier");
 
         foreach (GameObject soldier in otherSoldiers)
         {
-            if ((string)soldier.GetComponent<PhotonView>().Owner.CustomProperties["color"] != (string)gameObject.GetComponent<PhotonView>().Owner.CustomProperties["color"] && soldier.transform.position.y == gameObject.transform.position.y && Mathf.Abs(soldier.transform.position.x - gameObject.transform.position.x) < soldierRange)
+            if (!isDead && (string)soldier.GetComponent<PhotonView>().Owner.CustomProperties["color"] != (string)gameObject.GetComponent<PhotonView>().Owner.CustomProperties["color"] && soldier.transform.position.y == gameObject.transform.position.y && Mathf.Abs(soldier.transform.position.x - gameObject.transform.position.x) < soldierRange)
             {
                 animator.SetBool("IsAttacking", true);
+
+                if (attackTimer > attackRate && (string)PhotonNetwork.LocalPlayer.CustomProperties["color"] == "red")
+                {
+                    GiveDamage(soldier);
+                    attackTimer = 0.0f;
+                }
+                
                 return;
             }
             else
@@ -61,5 +84,42 @@ public class SoldierMovementController : MonoBehaviourPunCallbacks
         {
             gameObject.transform.Translate(directionOfMovementBlue);
         }
+    }
+
+    public void GiveDamage(GameObject soldier)
+    {
+        StartCoroutine(TakeDamageWaiter());
+        if (gameObject != null)
+        {
+            soldier.GetComponent<PhotonView>().RPC("TakeDamage", RpcTarget.All, soldierDamage);
+        }        
+    }
+
+    [PunRPC]
+    public void TakeDamage(float damage)
+    {
+        soldierHealth -= damage;
+    }
+
+    public void Die()
+    {
+        isDead = true;
+        animator.SetBool("IsDying", true);
+        StartCoroutine(DieWaiter());
+
+        if (photonView.IsMine)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }        
+    }
+
+    IEnumerator TakeDamageWaiter()
+    {
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1);
+    }
+
+    IEnumerator DieWaiter()
+    {
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1);
     }
 }
